dfc9a3916e0226d19c0cdb7d2180fd5a38fee5f4
kat-dearstyne <katherine@jklfamily.com>
2022-02-10 19:47:35 -0500

    this is a mess, please never look at this commit

--- cleaning/dataset.py
+++ cleaning/dataset.py
@@ -4,17 +4,17 @@ import tensorflow as tf
 
 class Dataset:
 
-    def __init__(self, dataset_dir, labels=None, follow_links=False):
+    def __init__(self, dataset_dir, labels=None, follow_links=False, label_mode=None):
         self.dataset_dir = dataset_dir
         self.labels = labels
+        self.label_mode = label_mode
         self.follow_links = follow_links
         self._training_data = None
         self._validation_data = None
 
     def get_params(self, subset="training"):
-        label_mode = "categorical" if self.labels is not None else self.labels
         return {'directory': self.dataset_dir, 'validation_split': VALIDATION_SPLIT, 'subset': subset,
-                'color_mode': 'rgb', 'image_size': IMAGE_SIZE, 'label_mode': label_mode, 'labels': self.labels,
+                'color_mode': 'rgb', 'image_size': IMAGE_SIZE, 'label_mode': self.label_mode, 'labels': self.labels,
                 'shuffle': True, 'seed': RANDOM_SEED, 'batch_size': BATCH_SIZE, 'follow_links': self.follow_links}
 
     def get_training_data(self):
@@ -24,7 +24,7 @@ class Dataset:
             )
         return self._training_data
 
-    def get_validation_data(self):
+    def get_testing_data(self):
         if self._validation_data is None:
             self._validation_data = tf.keras.utils.image_dataset_from_directory(
                 **self.get_params("validation")
--- cleaning/volume_dataset.py
+++ cleaning/volume_dataset.py
@@ -1,10 +1,59 @@
 import os
 from cleaning.dataset import Dataset
 from constants import DATA_DIR
+import csv
+from ingredient import Ingredient
+import pandas as pd
 
 
 class VolumeDataset(Dataset):
     volume_dir = os.path.join(DATA_DIR, 'volume')
+    nutrition_dir = os.path.join(volume_dir, 'nutrition5k')
+    eucstfd_dir = os.path.join(volume_dir, 'eucstfd')
+    name_index = 0
+    calorie_index = 1
+    mass_index = 2
+    num_features = 6
+    metadata_file1 = os.path.join(nutrition_dir, 'dish_metadata_cafe2.csv')
+    density_file = os.path.join(eucstfd_dir, 'density.xls')
+    image_ingredient_mappings = None
+    label_to_remove = "ingr_"
+    food_types = ['apple', 'banana', 'bread', 'bun', 'doughnut', 'egg', 'fired_dough_twist', 'grape', 'lemon', 'litchi', 'mango',
+                  'mix', 'mooncake', 'orange', 'pear', 'peach', 'plum', 'qiwi', 'sachima', 'tomato']
 
     def __init__(self, follow_links=False):
         super().__init__(self.volume_dir, follow_links=follow_links)
+
+    def get_image_ingredient_mappings(self):
+        if self.image_ingredient_mappings is None:
+            image_ingredient_mappings = {}
+            with open(self.metadata_file1, newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    new_row = [item for item in row if self.label_to_remove not in item]
+                    ingredients = []
+                    num_ingredients = int((len(new_row) / self.num_features) - 1)
+                    for i in range(1, num_ingredients):
+                        index = i * self.num_features
+                        name = row[index + self.name_index]
+                        calories = row[index + self.calorie_index]
+                        mass = row[index + self.mass_index]
+                        ingredients.append(Ingredient(name, calories, mass))
+                    image_ingredient_mappings[row[self.name_index]] = ingredients
+        return self.image_ingredient_mappings
+
+    def get_volume_info(self):
+        dataframes = pd.read_excel(self.density_file, sheet_name=self.food_types, index_col=False)
+        df = pd.concat(dataframes.values(), ignore_index=True)
+        return df
+
+    def get_calories_for_label(self, id_):
+        try:
+            calories = self.get_volume_info().loc[id_]['weight(g)']
+        except KeyError:
+            calories = 0
+        return calories
+
+
+if __name__ == "__main__":
+    print(VolumeDataset().something())
